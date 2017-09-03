93fcbaf9e2e97067642d9979e481bbd0dbec5142
SriramKeerthi <shramk@gmail.com>
2017-09-03 08:46:46 +0530

    Prim's

--- dfsbfs.py
+++ dfsbfs.py
@@ -18,6 +18,16 @@ class Graph():
             self.graph[v1] = []
         edgeList = self.graph[v1]
         edgeList.append((v2, w))
+
+    def addUnorderedEdge(self, v1, v2, w = 1):
+        if v1 not in self.graph:
+            self.graph[v1] = []
+        edgeList = self.graph[v1]
+        edgeList.append((v2, w))
+        if v2 not in self.graph:
+            self.graph[v2] = []
+        edgeList = self.graph[v2]
+        edgeList.append((v1, w))
         
     def traverseDFS(self, v):
         visited = OrderedDict()
@@ -91,15 +101,42 @@ class Graph():
                 edgeList = reverse[v]
                 edgeList.append((key, w))
         return reverse
+                
+    def _minKey(self, mstSet, keyValue):
+        minkey = None
+        minweight = None
+        for key,weight in keyValue.items():
+            if key not in mstSet and (minweight is None or minweight > weight):
+                minweight = weight
+                minkey = key
+        return minkey
         
+    def primMst(self):
+        mstSet = set()
+        keys = list(self.graph.keys())
+        keyValue = OrderedDict()
+        keyValue[keys[0]] = 0
+        path = {}
+        while len(mstSet) != len(keys):
+            u = self._minKey(mstSet, keyValue)
+            mstSet.add(u)
+            if u in self.graph:
+                for (v,w) in self.graph[u]:
+                    if v not in mstSet:
+                        if v not in keyValue or keyValue[v] > w:
+                            keyValue[v] = w
+                            path[v] = (u,v,w)
+                    
+        return list(path.values())
+            
 def main():
     random.seed(1)
     g = Graph()
-    for v in ['A', 'B', 'C', 'D', 'E']:
-        for u in ['A', 'B', 'C', 'D', 'E']:
-            if v != u:
-                if random.randint(0, 1) == 0:
-                    g.addEdge(v, u)
+#    for v in ['A', 'B', 'C', 'D', 'E']:
+#        for u in ['A', 'B', 'C', 'D', 'E']:
+#            if v != u:
+#                if random.randint(0, 1) == 0:
+#                    g.addEdge(v, u, random.randint(1,3))
     
 #    g.addEdge('A', 'B')
 #    g.addEdge('B', 'C')
@@ -115,9 +152,35 @@ def main():
 #    g.addEdge('J', 'G')
 #    g.addEdge('J', 'K')
 #    
+    g.addUnorderedEdge("0", "1", 4)
+    g.addUnorderedEdge("0", "7", 8)
+    g.addUnorderedEdge("1", "7", 11)
+    g.addUnorderedEdge("1", "2", 8)
+    g.addUnorderedEdge("7", "8", 7)
+    g.addUnorderedEdge("6", "7", 1)
+    g.addUnorderedEdge("2", "8", 2)
+    g.addUnorderedEdge("6", "8", 6)
+    g.addUnorderedEdge("2", "3", 7)
+    g.addUnorderedEdge("2", "5", 4)
+    g.addUnorderedEdge("5", "6", 2)
+    g.addUnorderedEdge("3", "5", 14)
+    g.addUnorderedEdge("3", "4", 9)
+    g.addUnorderedEdge("4", "5", 10)
+    
+#    g.addUnorderedEdge(0, 1, 2)
+#    g.addUnorderedEdge(0, 3, 6)
+#    g.addUnorderedEdge(1, 2, 3)
+#    g.addUnorderedEdge(1, 4, 5)
+#    g.addUnorderedEdge(1, 3, 8)
+#    g.addUnorderedEdge(2, 4, 7)
+#    g.addUnorderedEdge(3, 4, 9)
+    
+    
     
     print(g.graph)
-    print(g.stronglyConnected())
-    print(g.traverseDFS('D'))
+#    print(g.stronglyConnected())
+#    print(g.traverseDFS('D'))
+    print(g.primMst())
+    
 if __name__ == "__main__":
     main()
